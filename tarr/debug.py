a3d77f477d29c7384ca074e7fafe9a15a3e7d7e7
Krisztian Fekete <fkr972@gmail.com>
2012-10-03 11:23:01 +0200

    tarr.debug.WRITE_TO_FILE(filename[, data_formatter])

---
+++ tarr/debug.py
@@ -0,0 +1,26 @@
+# drop data to file filter
+import tarr.compiler_base
+
+
+def format_data(data):
+    return '{0.id}: {0.payload}'.format(data)
+
+
+class WRITE_TO_FILE(tarr.compiler_base.Instruction):
+
+    @property
+    def __name__(self):
+        return 'POINT OF INTEREST - WRITE("{}")'.format(self.filename)
+
+    def __init__(self, filename, formatter=format_data):
+        self.format = formatter
+        self.filename = filename
+
+    def run(self, runner, data):
+        # NOTE: we need to do writing in UNBUFFERED mode (buffering=0)
+        # as potentially there are other processes writing to the same file *NOW*
+        with open(self.filename, 'ab', buffering=0) as f:
+            f.write(self.format(data) + '\n')
+
+    def clone(self):
+        return self.__class__(filename=self.filename, formatter=self.format)
---
+++ tarr/test_debug.py
@@ -0,0 +1,25 @@
+import unittest
+import os.path
+import tempdir
+import tarr.debug as m
+from tarr.compiler import Program, RETURN
+from tarr.data import Data
+
+
+class Test_WRITE_TO_FILE(unittest.TestCase):
+
+    def program(self, tempfile):
+        return Program([m.WRITE_TO_FILE(tempfile), RETURN])
+
+    def test_writes_data_as_id_and_payload(self):
+        with tempdir.TempDir() as d:
+            tempfile = os.path.join(d.name, 'tempfile')
+            p = self.program(tempfile)
+            p.run(Data('id', 'payload'))
+            p.run(Data(1, 'Data'))
+
+            with open(tempfile) as f:
+                self.assertEqual(
+                    ['id: payload\n',
+                    '1: Data\n'],
+                    f.readlines())
