128474fc262d5b1af935661f19f6e752df888bc2
Krisztian Fekete <fkr972@gmail.com>
2013-05-30 12:26:26 +0200

    First public release for mETL testing

--- setup.py
+++ setup.py
@@ -4,6 +4,6 @@ from setuptools import setup
 setup(
     name='TARR',
     description='A non-parallel data flow language',
-    version='0.1.0-dev',
+    version='0.1.1',
     packages=['tarr'],
 )
